f80017e570985fb802f3545cf874bad07ad4dbb2
Raviravirego76466 <raviravirego@gmail.com>
2025-06-21 19:10:57 -0300

    Update IsotopeReactor.java

--- src/isotope/types/IsotopeReactor.java
+++ src/isotope/types/IsotopeReactor.java
@@ -6,7 +6,7 @@ import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.struct.*;
 import arc.util.*;
-import betamindy.content.*;
+import isotope.content.*;
 import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.game.*;
